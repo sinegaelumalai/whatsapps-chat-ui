54775c6283b47ad738fcfeaafc91600ab158c47e
Snega <sinegaelumalai@gmail.com>
2026-05-29 13:21:03 +0530

    Fix React app

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter> 
       <Routes>
         <Route path="/" element={<Layout />}>
 
